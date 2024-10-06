3c017fe5b14d5a4a2b776ca7d3d0a2aa5dba2c06
Rohan <rohan.mokashi7@gmail.com>
2024-10-06 10:33:36 +0530

    Swap of two numbers using ExOr operator

---
+++ src/com/question1/SwapOfTwoNumberExOr.java
@@ -0,0 +1,20 @@
+package com.question1;
+
+public class SwapOfTwoNumberExOr {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		a=a^b;
+		System.out.println(a);
+		
+		b=a^b;
+		System.out.println(b);
+		
+		a=a^b;
+		System.out.println(a);
+
+	}
+
+}
